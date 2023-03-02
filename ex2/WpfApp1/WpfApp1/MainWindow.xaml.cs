355e2b3f14e17a4444c9076a47f4c58c4093a5e1
PawelManczak <pawel14522@gmail.com>
2023-03-02 21:47:58 +0100

    ex 2 - 1 edited

--- ex2/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ ex2/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,8 +1,8 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Forms;
-using WpfApp1.treeView;
 using MessageBox = System.Windows.MessageBox;
-using TreeView = System.Windows.Forms.TreeView;
 using Window = System.Windows.Window;
 
 namespace WpfApp1
@@ -15,23 +15,6 @@ namespace WpfApp1
             InitializeComponent();
 
         }
-        private static void PopulateTreeView(TreeView treeView, string[] paths, char pathSeparator)
-        {
-            //Creates a tree from given path list
-            foreach (string path in paths)
-            {
-                TreeNodeCollection nodes = treeView.Nodes;
-
-                foreach (string path_part in path.Split('\\'))
-                {
-                    //Here it adds a new node (file or folder)
-                    if (!nodes.ContainsKey(path_part))
-                        nodes.Add(path_part, path_part);
-                    //Go one node deeper
-                    nodes = nodes[path_part].Nodes;
-                }
-            }
-        }
         public void toolbarOpen_Click(object sender, RoutedEventArgs e)
         {
             using (FolderBrowserDialog dialog = new FolderBrowserDialog() { Description = "Select directory to open" })
@@ -40,23 +23,47 @@ namespace WpfApp1
                 {
                     string path = dialog.SelectedPath;
                     MessageBox.Show(path);
-                    string[] paths = new string[1];
-                    paths[0] = path;
-                    var itemProvider = new ItemProvider();
 
-                    var items = itemProvider.GetItems(path);
 
-                    DataContext = items;
                     //create a tree
-                    // PopulateTreeView(this.myTreeViewEvent, paths, '/');
+                    var root = new TreeViewItem
+                    {
+                        Header = Path.GetFileName(path),
+                        Tag = Path.GetDirectoryName(path)
+                    };
+
+                    DirectoryInfo dir = new DirectoryInfo(path);
+                    createATree(root, dir);
+
+                    this.tree.Items.Add(root);
                 }
             }
 
         }
 
+        void createATree(TreeViewItem root, DirectoryInfo dir)
+        {
+            foreach (var item in dir.EnumerateFileSystemInfos())
+            {
+                TreeViewItem tvi = new TreeViewItem()
+                {
+                    Header = item.Name,
+                    Tag = Path.GetDirectoryName(item.FullName)
+                };
+
+                if (item is DirectoryInfo)
+                {
+                    createATree(tvi, item as DirectoryInfo);
+                }
+
+                root.Items.Add(tvi);
+            }
+        }
         public void toolbarExit_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
+
+
     }
 }
