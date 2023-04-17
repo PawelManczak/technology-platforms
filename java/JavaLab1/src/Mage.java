105bc4afd33464d5f1c3c8d2bf0c29f0ace5d851
PawelManczak <pawel14522@gmail.com>
2023-04-17 18:26:35 +0200

    sets moved into class

--- java/JavaLab1/src/Mage.java
+++ java/JavaLab1/src/Mage.java
@@ -1,18 +1,28 @@
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class Mage implements Comparable<Mage> {
     private String name;
     private int level;
     private double power;
-    private Set<Mage> apprentices = new HashSet<>();
+    private Set<Mage> apprentices;
+    private Map<Mage, Integer> map;
+    private String sortMode;
 
-    public Mage(String name, int level, double power) {
+    public Mage(String name, int level, double power, String sortMode) {
          this.name = name;
          this.level = level;
          this.power = power;
 
+         this.sortMode = sortMode;
+        if (sortMode.equals("natural")) {
+            apprentices = new TreeSet<>();
+        } else if (sortMode.equals("alternative")) {
+            Comparator<Mage> alternativeComparator = Comparator.comparing(Mage::getLevel)
+                    .thenComparing(Mage::getPower);
+            apprentices = new TreeSet<>(alternativeComparator);
+        } else {
+            apprentices = new HashSet<>();
+        }
     }
 
     @Override
@@ -65,6 +75,17 @@ public class Mage implements Comparable<Mage> {
         }
     }
 
+    public int getAmountOfApprentice(Mage mMage){
+        int counter = 1;
+        for(Mage mage : mMage.apprentices){
+            counter += getAmountOfApprentice(mage);
+        }
+        return counter;
+    }
+
+    public int getAmountOfApprentice(){
+        return getAmountOfApprentice(this) - 1;
+    }
     public void addApprentice(Mage mag) {
         this.apprentices.add(mag);
     }
@@ -75,5 +96,38 @@ public class Mage implements Comparable<Mage> {
 
     public int getLevel() {
         return level;
+    }
+
+    public void printStatistics(){
+
+        if (sortMode.equals("natural")) {
+            map = new TreeMap<>();
+        } else if (sortMode.equals("alternative")) {
+            map = new TreeMap<>();
+        } else {
+            map = new HashMap<>();
+        }
+
+        map.put(this, this.getAmountOfApprentice());
+        get(this);
+
+        for (Map.Entry<Mage, Integer> entry : map.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ". Value: " + entry.getValue());
+        }
+    }
+
+    private void get(Mage mage1){
+
+
+        for(Mage mage : mage1.apprentices){
+            get(mage);
+            map.put(mage, mage.getAmountOfApprentice());
+        }
+    }
+
+    public void printApprentices(){
+        for (Mage mage : apprentices) {
+            System.out.println(mage);
+        }
     }
 }
--- java/JavaLab1/src/Main.java
+++ java/JavaLab1/src/Main.java
@@ -1,64 +1,51 @@
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 // Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Set<Mage> mages;
-        String sortMode = "natural";
+        String sortMode = "alternative";//alternative // natural
 
-        // Tworzenie obiektów klasy Mage
-        Mage mage1 = new Mage("Gandalf", 20, 100.0);
-        Mage mage2 = new Mage("Saruman", 18, 95.0);
-        Mage mage3 = new Mage("Merlin", 15, 85.0);
-        Mage mage4 = new Mage("Harry Potter", 10, 80.0);
-        Mage mage5 = new Mage("Dumbledore", 20, 90.0);
-        Mage mage6 = new Mage("Gargamel", 5, 70.0);
-        Mage mage7 = new Mage("Hermione Granger", 12, 75.0);
-        Mage mage8 = new Mage("Mickey Mouse", 7, 60.0);
-        Mage mage9 = new Mage("Dr. Strange", 18, 98.0);
-        Mage mage10 = new Mage("Gonzo", 3, 50.0);
+        Mage mage1 = new Mage("Gandalf", 20, 100.0, sortMode);
+        Mage mage2 = new Mage("Saruman", 18, 95.0, sortMode);
+        Mage mage3 = new Mage("Merlin", 15, 85.0, sortMode);
+        Mage mage4 = new Mage("Harry Potter", 10, 80.0, sortMode);
+        Mage mage5 = new Mage("Dumbledore", 20, 90.0, sortMode);
+        Mage mage6 = new Mage("Gargamel", 5, 70.0, sortMode);
+        Mage mage7 = new Mage("Hermione Granger", 12, 75.0, sortMode);
+        Mage mage8 = new Mage("Mickey Mouse", 7, 60.0, sortMode);
+        Mage mage9 = new Mage("Dr. Strange", 18, 98.0, sortMode);
+        Mage mage10 = new Mage("Gonzo", 3, 50.0, sortMode);
 
         mage1.addApprentice(mage2);
         mage1.addApprentice(mage3);
-        mage3.addApprentice(mage10);
+        mage1.addApprentice(mage4);
+        mage1.addApprentice(mage5);
+
+        mage5.addApprentice(mage6);
+        mage5.addApprentice(mage7);
+        mage5.addApprentice(mage8);
+        mage5.addApprentice(mage9);
+
+
+        /*mage3.addApprentice(mage10);
         mage10.addApprentice(mage9);
         mage5.addApprentice(mage4);
         mage5.addApprentice(mage7);
-        mage9.addApprentice(mage8);
+        mage9.addApprentice(mage8);*/
 
-        if (sortMode.equals("natural")) {
-            mages = new TreeSet<>();
-        } else if (sortMode.equals("alternative")) {
-            Comparator<Mage> alternativeComparator = Comparator.comparing(Mage::getLevel)
-                    .thenComparing(Mage::getPower);
-            mages = new TreeSet<>(alternativeComparator);
-        } else {
-            mages = new HashSet<>();
-        }
-
-        mages.add(mage1);
-        mages.add(mage2);
-        mages.add(mage3);
-        mages.add(mage4);
-        mages.add(mage5);
-        mages.add(mage6);
-        mages.add(mage7);
-        mages.add(mage8);
-        mages.add(mage9);
-        mages.add(mage10);
-
-
-        for (Mage mage : mages) {
-            System.out.println(mage);
-        }
-
-
-        System.out.println("\n\n\n");
+        System.out.println("print info about Gandalf");
         mage1.print();
+        System.out.println("\n");
+
+        System.out.println("Gandalf statistics");
+        mage1.printStatistics();
+
+        System.out.println("\namount of Gandalfs apprentices");
+        System.out.println(mage1.getAmountOfApprentice());
+
+        System.out.println("\napprentices of Gandalf");
+        mage1.printApprentices();
     }
 }
 
